4f3a79586a594a125a172e214fd4f2639e483743
Niklas Terhoeven <niklas.terhoeven@uni-wuerzburg.de>
2018-01-22 11:51:23 +0100

    add -n parameter to maker2zff

--- annotation/get_zff.sh
+++ annotation/get_zff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/storage/compevolbiol/software/Maker/maker/bin/maker2zff -d av-canu-pilon.maker.output/av-canu-pilon_master_datastore_index.log
+/storage/compevolbiol/software/Maker/maker/bin/maker2zff -n -d av-canu-pilon.maker.output/av-canu-pilon_master_datastore_index.log
