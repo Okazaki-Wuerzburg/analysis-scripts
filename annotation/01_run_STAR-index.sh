ac9363bcb236bd79d5aa727f6440bb600549d8b4
Niklas Terhoeven <niklas.terhoeven@uni-wuerzburg.de>
2018-02-15 13:42:02 +0100

    increase RAM limit to 80G

--- annotation/01_run_STAR-index.sh
+++ annotation/01_run_STAR-index.sh
@@ -4,4 +4,4 @@ threads=$1
 genome=$2
 
 mkdir ./genomeIndex
-/storage/compevolbiol/software/star/bin/Linux_x86_64/STAR --runThreadN "$threads" --runMode genomeGenerate --genomeDir ./genomeIndex --genomeFastaFiles "$genome"
+/storage/compevolbiol/software/star/bin/Linux_x86_64/STAR --runThreadN "$threads" --limitGenomeGenerateRAM 80000000000 --runMode genomeGenerate --genomeDir ./genomeIndex --genomeFastaFiles "$genome"
