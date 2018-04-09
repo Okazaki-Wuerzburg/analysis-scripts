5cf03a1979e236be6f8a88177f1496e9bc22f03a
Niklas Terhoeven <niklas.terhoeven@uni-wuerzburg.de>
2018-04-09 09:18:29 +0200

    adding a third iteration and tutorial style options

---
+++ annotation/12_run_Interproscan.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/storage/compevolbiol/software/Interproscan/interproscan-5.25-64.0/interproscan.sh --cpu 30 --goterms --input $1
---
+++ annotation/13_run_snap-2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+export ZOE="/storage/compevolbiol/software/snap/snap/Zoe"
+export PATH="/storage/compevolbiol/software/snap/snap/":$PATH
+
+echo "######################"
+date
+echo "create zff files from maker gffs"
+echo ""
+/storage/compevolbiol/software/Maker/maker/bin/maker2zff -n maker-1.gff maker-2.gff
+
+echo "######################"
+date
+echo "gene stats"
+echo ""
+fathom genome.ann genome.dna -gene-stats
+
+
+echo "######################"
+date
+echo "validate"
+echo ""
+fathom genome.ann genome.dna -validate
+
+echo "######################"
+date
+echo "categorize genes"
+echo ""
+fathom genome.ann genome.dna -categorize 1000
+
+echo "######################"
+date
+echo "convert strand"
+echo ""
+fathom uni.ann uni.dna -export 1000 -plus
+
+echo "######################"
+date
+echo "parameter estimation"
+echo ""
+mkdir params
+cd params
+forge ../export.ann ../export.dna
+cd ..
+
+echo "######################"
+date
+echo "build HMM"
+echo ""
+hmm-assembler.pl my-genome params > my-genome.hmm
